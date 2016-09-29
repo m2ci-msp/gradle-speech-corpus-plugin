8e784bf9b711efa33d1bd4633a9090490ad58971
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-09-29 05:02:26 +0200

    refactor test code

--- src/test/groovy/org/m2ci/msp/speechcorpus/SpeechCorpusPluginTest.groovy
+++ src/test/groovy/org/m2ci/msp/speechcorpus/SpeechCorpusPluginTest.groovy
@@ -1,16 +1,21 @@
 package org.m2ci.msp.speechcorpus
 
-import org.testng.annotations.Test
+import org.testng.annotations.*
 
-import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 
 class SpeechCorpusPluginTest {
 
-    @Test
-    public void canApplyPlugin() {
-        Project project = ProjectBuilder.builder().build()
+    def project
+
+    @BeforeTest
+    void setUp() {
+        project = ProjectBuilder.builder().build()
         project.pluginManager.apply 'org.m2ci.msp.speech-corpus'
+    }
+
+    @Test
+    void canApplyPlugin() {
         assert project.pluginManager.hasPlugin('org.m2ci.msp.speech-corpus')
     }
 }
